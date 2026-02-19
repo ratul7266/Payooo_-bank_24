ae5cf7cc49c3c27a66316e22be3abd8e4a8022b3
MD Ratul <tahid7266@gmail.com>
2026-02-19 16:05:38 +0600

    Implement Login Logic And navigate to Homepage

---
+++ script/login.js
@@ -0,0 +1,22 @@
+console.log("conneting");
+
+document.getElementById("login-btn").addEventListener("click", function () {
+  // input number
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+
+  // input pin
+  const pinInput = document.getElementById("input-pin");
+  const pinNumber = pinInput.value;
+
+  // match input
+  if (contactNumber == "01972610566" && pinNumber == "7266") {
+    alert("Login Sucess");
+
+    // window.location.replace("/home.html")
+    window.location.assign("/home.html")
+  } else {
+    alert("Something is Wrong || Login failed || Error 404!!!");
+    return;
+  }
+});
