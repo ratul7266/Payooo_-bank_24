5e4e3d3693cf1a40f07fb0ab90876a5f360a0162
MD Ratul <tahid7266@gmail.com>
2026-02-20 15:20:34 +0600

    small bug fix

--- script/login.js
+++ script/login.js
@@ -14,7 +14,7 @@ document.getElementById("login-btn").addEventListener("click", function () {
     alert("Login Sucess");
 
     // window.location.replace("/home.html")
-    window.location.assign("/home.html");
+    window.location.assign("../home.html");
   } else {
     alert("Something is Wrong || Login failed || Error 404!!!");
     return;
